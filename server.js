55800ceda64f0b76dfd3c5d8ba165b58a2d33e3c
Cheryl Brady <cbrady80@byui.edu>
2023-04-26 14:05:40 -0600

    added call to init mongoDB

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ const port = 5000;
 // Calls the routes to view the data
 app.use('/', require('./routes/index'));
 
-// Event Listener
-app.listen(process.env.port || port);
-
-// Log message
-console.log('Web Server is listening at port ' + (process.env.port || port));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        // Event Listener
+        app.listen(process.env.port || port);
+        // Log message
+        console.log('Web Server is listening at port ' + (process.env.port || port));
+    }
+});

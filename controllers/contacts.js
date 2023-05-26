4d7e79a4b02e3fec68f0cb52b46f301f993bc45a
Cheryl Brady <cbrady80@byui.edu>
2023-05-25 22:15:30 -0600

    fixed errors in controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,18 +6,16 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Function to retrieve all contacts
 const getAllContacts = async (req, res, next) => {
-    mongodb
+    const result = await mongodb
         .getDb()
         .db('test')
         .collection('contacts')
-        .find()
-        .toArray((err, lists) => { //changed this to add in the error handling for week 6
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
+        .find();
+        result.toArray().then(
+            lists => {res.setHeader('Content-Type', 'application/json'); 
+                      res.status(200).json(lists)},
+            err => {res.status(400).json({ message: err })}
+        );
 };
 
 // Function to retrive one contact by ID
@@ -26,18 +24,16 @@ const getContactById = async (req, res, next) => {
         res.status(400).json('Must use a valid contact id to find a contact.');
     }
     const userId = new ObjectId(req.params.id);
-    mongodb
+    const result = await mongodb
         .getDb()
         .db('test')
         .collection('contacts')
-        .find({_id: userId})
-        .toArray((err, result) => {  //changed this to add in the error handling for week 6
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(result[0]);
-        });
+        .find({_id: userId});
+        result.toArray().then(
+            lists => {res.setHeader('Content-Type', 'application/json'); 
+                      res.status(200).json(lists[0])},
+            err => {res.status(400).json({ message: err })}
+        );
 };
 
 // Function to create a NEW contact
